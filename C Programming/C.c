d967ed77fb3ba8a89fffffadcd6439f976c30561
Pawan Rama Mali <prm@outlook.in>
2021-02-16 01:56:31 +0530

    Version 1.0

---
+++ C Programming/C.c
@@ -0,0 +1,15 @@
+/*stdio.h is a library that operates input output functions of the program */
+#include<stdio.h>
+/*The program is written in the main function,this main does not return a value*/
+void main()
+{
+	// Using printf to send message to console out
+	printf("Hello World!");
+}
+/*The program is written in the main function,this main does not return a value*/
+int main()
+{
+	// Using printf to send message to console out
+	printf("Hello World!");
+	return 0;
+}
